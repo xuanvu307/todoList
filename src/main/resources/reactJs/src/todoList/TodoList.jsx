46ec8c9147b83fcd79af1fea290ddaa18d2881c3
xuanvu307 <xuanvufb2@gmail.com>
2023-03-13 21:30:44 +0700

    Update TodoList.jsx

--- src/main/resources/reactJs/src/todoList/TodoList.jsx
+++ src/main/resources/reactJs/src/todoList/TodoList.jsx
@@ -10,31 +10,28 @@ function TodoList() {
     useEffect(() =>{
         const fetchData = async () =>{
             const data = await fetch(`${URL}`)
-            const dataJson = await data.json();   
+            const dataJson = await data.json();  
             setTodos(dataJson)
         }
         fetchData();
     },[])
 
-    const handleChangeInput = (event) =>{
-        SetInputValue(event.target.value)
-    }
-
-    const addTodo = async () =>{
+    const addTodo =  async () =>{
         if(inputValue.trim() === ""){
             alert("Tiêu đề không để trống");
             return;
         }
-
-        await fetch(`${URL}`, 
+        let todo = await fetch(`${URL}`, 
             {method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({title: `${inputValue}`})
         })
-        setTodos([...todos,inputValue]);
+        const dataJson = await todo.json();
+
+        setTodos([...todos,dataJson]);
     }
 
-    const editTodo =async (index) =>{
+    const editTodo = async (index) =>{
         const titleName = prompt("Nhập tiêu đề cần sửa");
         if (titleName.trim() === ""){
             alert("Tiêu đề trống");
@@ -43,12 +40,23 @@ function TodoList() {
         const status = prompt("Hoàn thành nhập true ");
         const getStatus = status.trim().toLowerCase() == "true" ? "true" : "false";
 
-        await fetch(`${URL}/${index}`,
+        const todo = await fetch(`${URL}/${index}`,
             {method : 'PUT',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({title: titleName, status : getStatus})
         })
+
+        const newTodo = await todo.json();
+        console.log(newTodo)
+        const newTodos = todos.map(e=>{
+            if(e.id == index){
+                return newTodo
+            }
+            return e;
+        })
+        setTodos(newTodos);
     }
+
 
     const deleteTodo = async (index) =>{
         await fetch(`${URL}/${index}`, {method : 'DELETE'});
@@ -58,7 +66,8 @@ function TodoList() {
 
     const changeChecked = async (index) => {
         await fetch(`${URL}/toggle/${index}`, {
-            method:'PUT'
+            method:'PUT',
+            headers: { 'Content-Type': 'application/none' }
         })
         const newTodos = todos.map(e => {
             if (e.id === index){
@@ -68,17 +77,21 @@ function TodoList() {
         setTodos(newTodos);
     }
     
+    useEffect(() =>{
+        console.log("render")
+    },[todos,inputValue])
+
   return (
     <>
         <h1>TodoList</h1>
-        <input type="text" placeholder='Add Công việc mới' value={inputValue} onChange={handleChangeInput}/>
+        <input type="text" placeholder='Add Công việc mới' value={inputValue} onChange={e => SetInputValue(e.target.value)}/>
         <button onClick={addTodo}>Add</button>
         {todos.length > 0 ? 
             <ul>
                 {todos.map((e, i) =>(
                     <li key={i}>
-                        <input type="checkbox" name="" id="" onChange = {() => changeChecked(e.id)}  checked = {e.status}/>
-                        {e.title}
+                        <input type="checkbox" name="" id="" onChange = {() => changeChecked(e?.id)}  checked = {e?.status}/>
+                        {e?.title}
                         <button onClick={() =>editTodo(e.id)}>Edit</button>
                         <button onClick={() => deleteTodo(e.id)}>Delete</button>
                     </li>
