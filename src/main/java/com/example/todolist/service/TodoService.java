59481b111f09d99a0f52ce3dfed0520ca19ff6fe
xuanvu307 <xuanvufb2@gmail.com>
2023-03-23 15:17:00 +0700

    Update TodoService.java

--- src/main/java/com/example/todolist/service/TodoService.java
+++ src/main/java/com/example/todolist/service/TodoService.java
@@ -9,37 +9,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
-import java.util.Random;
 
 @Service
 public class TodoService {
-
-
     @Autowired
     private TodoRepository todoRepository;
 
-    private Integer genarateId() {
-        Random rd = new Random();
-        return rd.nextInt(1000);
-    }
-
     public List<Todo> getAllTodo() {
-//        return todoRepository.getAllTodo();
         return todoRepository.findAll();
     }
 
     public Todo getTodoById(Integer id) {
-//        return todoRepository.getAllTodo().stream()
-//                .filter(todo -> todo.getId() == id)
-//                .findFirst()
-//                .orElseThrow(() ->
-//                        new NotFoundException("id khong ton tai")
-//                )
-//                ;
-
-
         Optional<Todo> todoOptional = todoRepository.findById(id);
         if (todoOptional.isPresent()) {
             return todoOptional.get();
@@ -49,17 +30,7 @@ public class TodoService {
     }
 
     public Todo createTodo(CreateTodoRequest request) {
-//        Todo todo = Todo.builder()
-//                .id(genarateId())
-//                .title(request.getTitle())
-//                .status(false)
-//                .build();
-//        todoRepository.getAllTodo().add(todo);
-//        return todo;
-
-
         Todo todo = Todo.builder()
-                .id(genarateId())
                 .title(request.getTitle())
                 .status(false)
                 .build();
@@ -68,21 +39,14 @@ public class TodoService {
     }
 
     public Todo updateTodo(Integer id, UpdateTodoRequest request) {
-//        for (Todo todo : todoRepository.getAllTodo()) {
-//            if (todo.getId().equals(id)) {
-//                todo.setTitle(request.getTitle());
-//                todo.setStatus(request.getStatus());
-//                return todo;
-//            }
-//        }
-//        throw new NotFoundException("id khong dung");
-
-
         Optional<Todo> todoOptional = todoRepository.findById(id);
+
         if (todoOptional.isPresent()) {
             Todo todo = todoOptional.get();
             todo.setTitle(request.getTitle());
             todo.setStatus(request.getStatus());
+
+            todoRepository.save(todo);
             return todo;
         } else {
             throw new BadRequest("Không tìm thấy id");
@@ -90,13 +54,10 @@ public class TodoService {
     }
 
     public void deleteTodo(Integer id) {
-//        todoRepository.getAllTodo().removeIf(todo -> Objects.equals(todo.getId(), id));
-
         Optional<Todo> todoOptional = todoRepository.findById(id);
 
         if (todoOptional.isPresent()) {
-            Todo todo = todoOptional.get();
-            todoRepository.findAll().removeIf(todo1 -> Objects.equals(todo1, todo));
+            todoRepository.delete(todoOptional.get());
         } else {
             throw new BadRequest("Không tìm thấy id");
         }
